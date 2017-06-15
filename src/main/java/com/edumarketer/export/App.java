548f19125e1a52efee7badc3f313aaf6fced7d0c
fjcano <fjcanobailen@gmail.com>
2017-06-15 21:38:59 +0200

    Initial commit

---
+++ src/main/java/com/edumarketer/export/App.java
@@ -0,0 +1,13 @@
+package com.edumarketer.export;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
