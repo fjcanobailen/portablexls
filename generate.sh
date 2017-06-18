c6c61a0d95da09f598e84a955dde57afffa290dc
fjcano <fjcanobailen@gmail.com>
2017-06-18 18:48:01 +0200

    Rename summary

--- generate.sh
+++ generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Generate all types of documents
 
-java -jar target/portablexls-jar-with-dependencies.jar -t /Users/sp12571/Workspace/portablexls/src/main/resources/templateInformeSemanalGeneral.xls -s /Users/sp12571/Workspace/portablexls/src/main/resources/summary.csv -d /Users/sp12571/Workspace/portablexls/src/main/resources/dataInformeSemanalGeneral.csv -o informeSemanalGeneral.pdf
+java -jar target/portablexls-jar-with-dependencies.jar -t /Users/sp12571/Workspace/portablexls/src/main/resources/templateInformeSemanalGeneral.xls -s /Users/sp12571/Workspace/portablexls/src/main/resources/summaryInformeSemanalGeneral.csv -d /Users/sp12571/Workspace/portablexls/src/main/resources/dataInformeSemanalGeneral.csv -o informeSemanalGeneral.pdf
 java -jar target/portablexls-jar-with-dependencies.jar -t /Users/sp12571/Workspace/portablexls/src/main/resources/templateInformeSemanalCalificaciones.xls -d /Users/sp12571/Workspace/portablexls/src/main/resources/dataInformeSemanalCalificaciones.csv -o informeSemanalCalificaciones0_9.pdf
 java -jar target/portablexls-jar-with-dependencies.jar -t /Users/sp12571/Workspace/portablexls/src/main/resources/templateInformeSemanalCalificaciones.xls -i 10,19 -d /Users/sp12571/Workspace/portablexls/src/main/resources/dataInformeSemanalCalificaciones.csv -o informeSemanalCalificaciones10_19.pdf
 java -jar target/portablexls-jar-with-dependencies.jar -t /Users/sp12571/Workspace/portablexls/src/main/resources/templateInformeSemanalCalificaciones.xls -i 90,94 -d /Users/sp12571/Workspace/portablexls/src/main/resources/dataInformeSemanalCalificaciones.csv -o informeSemanalCalificaciones90_94.pdf
